f2d5fe6a35e83962e95d65811cd59b1e37c6671a
madan-ram <madan_ram@rocketmail.com>
2020-04-10 11:17:02 +0530

    tensorflow v2 support, and integration of basic training function

--- tbpp_ssd_train.py
+++ tbpp_ssd_train.py
@@ -130,6 +130,7 @@ def main(_):
     strategy = None
     if FLAGS.use_tpu == 1:
         resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu=TPU_ADDRESS)
+        tf.config.experimental_connect_to_cluster(resolver)
         tf.tpu.experimental.initialize_tpu_system(resolver)
         strategy = tf.distribute.experimental.TPUStrategy(resolver)
 
